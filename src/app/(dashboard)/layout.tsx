80c3617fdd79e9fd2da883f0a3eb0a63610b79d6
SwiftkeyX <pisanucaraleadkul.pasit@gmail.com>
2024-12-29 17:19:36 +0700

    step2-Chart

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,9 +13,9 @@ export default function RootLayout({
   return (
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-white px-3">
         <Link href="/" 
-        className="flex items-center justify-center lg:justify-start gap-2">
+        className="flex items-center justify-center lg:justify-start gap-2 my-4">
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">SchoolLama</span>
         </Link>
@@ -23,7 +23,7 @@ export default function RootLayout({
       </div>
 
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] ">
         <Navbar />
         {children}
       </div>
